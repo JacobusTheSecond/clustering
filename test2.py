74d9a003645c159099aea0106de7e1bb291d5311
Jacobus Conradi <conradi@cs.uni-bonn.de>
2024-03-05 14:53:37 +0100

    pybind and smaller set system

---
+++ test2.py
@@ -0,0 +1,16 @@
+import numpy as np
+import klcluster as kl
+
+trials = [np.genfromtxt("data/86_"+str(i)+".txt",delimiter=" ") for i in range(1,15)]
+cs=kl.Curves()
+for trial in trials:
+	cs.add(kl.Curve(trial))
+
+cc=kl.CurveClusterer()
+cc.initCurves(cs,1.25)
+cr = cc.greedyCover(10,1)
+for cluster in cr:
+	print("Center:",cluster.center().values())
+	print("Matching:")
+	print(cluster.matching())
+	print("------")
