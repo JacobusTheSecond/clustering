84e4c6befc0396c3e79b0ff3164c93d9528ca5da
bartlmaes0 <s16sbart@uni-bonn.de>
2024-06-19 00:25:13 +0200

    =accuracy, precision und recall hinzugefuegt

--- experiments/cmu.py
+++ experiments/cmu.py
@@ -1,14 +1,27 @@
 from cmuSolvers import KlClusterCMUSolver
 from cmuSolvers import AcaCMUSolver
+TAG = 2
+klClusterSolver = KlClusterCMUSolver(TAG)
+gmmSolver = AcaCMUSolver(TAG, "gmm")
+acaSolver = AcaCMUSolver(TAG, "aca")
+hacaSolver = AcaCMUSolver(TAG, "haca")
 
-klClusterSolver = KlClusterCMUSolver(1)
-gmmSolver = AcaCMUSolver(1, "gmm")
-acaSolver = AcaCMUSolver(1, "aca")
-hacaSolver = AcaCMUSolver(1, "haca")
 
 segmentsKlCluster = klClusterSolver.solve()
+print(klClusterSolver.calculateAccurcacy(segmentsKlCluster, TAG))
+
+print("P")
 segmentsGmm = gmmSolver.solve()
+print("P")
+
 segmentsAca = acaSolver.solve()
 segmentsHaca = hacaSolver.solve()
 
+print(f"Instanz {TAG}:")
+print(klClusterSolver.calculateAccurcacy(segmentsKlCluster, TAG), gmmSolver.calculateAccurcacy(segmentsGmm, TAG), 
+     acaSolver.calculateAccurcacy(segmentsAca, TAG), hacaSolver.calculateAccurcacy(segmentsHaca, TAG))
+print(klClusterSolver.calculateMacroPrecision(segmentsKlCluster, TAG), gmmSolver.calculateMacroPrecision(segmentsGmm, TAG), 
+     acaSolver.calculateMacroPrecision(segmentsAca, TAG), hacaSolver.calculateMacroPrecision(segmentsHaca, TAG))
+print(klClusterSolver.calculateMacroRecall(segmentsKlCluster, TAG), gmmSolver.calculateMacroRecall(segmentsGmm, TAG), 
+     acaSolver.calculateMacroRecall(segmentsAca, TAG), hacaSolver.calculateMacroRecall(segmentsHaca, TAG))
 klClusterSolver.plotSegmentsAndGT([segmentsKlCluster, segmentsGmm, segmentsAca, segmentsHaca], ["KlCluster", "Gmm", "Aca", "Haca"])
